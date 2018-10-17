f7df895edf1055ab3b1ae73d47efc73e97f16e0d
Josephine Schwabel <josephine.schwabel@gmail.com>
2018-10-17 16:00:40 -0400

    added services & routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,15 +8,27 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  selectedHero: Hero;
-  heroes = HEROES;
+  //selectedHero: Hero;
+  heroes: Hero[];
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero=hero;
+  // onSelect(hero: Hero): void {
+  //   this.selectedHero=hero;
+  // }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
+
+  // this was synchronous & only worked because we were returning mock data
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
+
 }
